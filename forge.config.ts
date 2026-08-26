11224d2cc12fc5d3afb9deb34d6ee726d9195ead
Lucas Dantas <142459897+lukazfivee@users.noreply.github.com>
2026-08-26 11:47:25 -0300

    release: nomear instalador 1.0.1

--- forge.config.ts
+++ forge.config.ts
@@ -20,7 +20,7 @@ const config: ForgeConfig = {
       name: 'ConstrutecOrcamentos',
       authors: 'Construtec Engenharia',
       description: 'Orçamentos profissionais, rápidos, seguros e offline para a Construtec Engenharia.',
-      setupExe: 'Construtec-Orcamentos-1.0.0-Setup.exe',
+      setupExe: 'Construtec-Orcamentos-1.0.1-Setup.exe',
       noMsi: true,
     }),
     new MakerZIP({}, ['darwin']),
